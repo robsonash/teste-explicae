c0460a953bed40f84addba6cc13c8ee8702acee4
robsonSampaio <robsoncs211@gmail.com>
2025-02-28 20:26:32 -0300

    feat: adicionando componente AccordionCursos em App.vue (Estrutura Inicial)

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,38 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div>
+      <h2>Cadernos / <span>Língua Portuguesa</span></h2>
+      <p v-if="loading">Carregando cursos...</p>
+      <AccordionCursos v-else :cursos="cursos" />
+    </div>
+  </div>
 </template>
 
 <script>
+import AccordionCursos from "@/components/AccordionCursos.vue";
+import { fetchCursos } from "@/services/mockApi";
 export default {
   name: "App",
+  components: {
+    AccordionCursos,
+  },
+  data() {
+    return {
+      cursos: [],
+      loading: true,
+    };
+  },
+  async created() {
+    this.cursos = await fetchCursos();
+    this.loading = false;
+  },
 };
 </script>
 
 <style>
+body {
+  background: #eff0f0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -16,5 +40,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h2 {
+  color: #a9abad;
+  text-align: left;
+}
+h2 span {
+  border-bottom: 5px solid #a505f0;
 }
 </style>
