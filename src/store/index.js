7399d1d705561761ae383d919af16eae01b2dca5
robsonSampaio <robsoncs211@gmail.com>
2025-03-02 15:54:15 -0300

    Feat: Adicionando store para cursos

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,31 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { api } from "@/services/api";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    cursos: [],
+  },
+  mutations: {
+    SET_CURSOS(state, cursos) {
+      state.cursos = cursos;
+    },
+  },
+  actions: {
+    async fetchCursos({ commit }) {
+      try {
+        const cursos = await api.get("/cursos");
+        const cursosFiltrados = cursos.filter((curso) => !curso.isSubGroup);
+        commit("SET_CURSOS", cursosFiltrados);
+      } catch (error) {
+        alert("Erro ao carregar cursos principais:", error);
+        console.error("Erro ao carregar cursos principais:", error);
+      }
+    },
+  },
+  getters: {
+    cursos: (state) => state.cursos,
+  },
 });
